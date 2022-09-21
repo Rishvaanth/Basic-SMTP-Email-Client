5f525cf6ab25dcd4e370d3db5093225cd2c5cdc8
Rishvaanth <rishvaantht1998@gmail.com>
2022-09-20 19:57:52 -0500

    Added project files for the assignment

--- test/subprocess_run_test.py
+++ test/subprocess_run_test.py
@@ -5,12 +5,12 @@ import subprocess
 # lsTest = subprocess.run(["ls", "-la"], capture_output=True, text=True)  #text gives the regular output instead of
 # byte output.
 
-lsTest = subprocess.run(["dig", "mx", "zoho.com"], capture_output=True, text=True)
-getServer = subprocess.run(["grep", "SERVER"], capture_output=True, text=True, input=lsTest.stdout)
+lsTest = subprocess.run(["host", "-t", "mx", "zoho.com"], capture_output=True, text=True)
+getServer = subprocess.run(capture_output=True, text=True, input=lsTest.stdout)
 # print("the output is:")
 print(lsTest.stdout)
 indexa = getServer.stdout.rfind("(")
-indexb = getServer.stdout.rfind(")")
+indexb = getServer.stdout.rfind(".")
 # print(indexa)
 # print(getServer.stdout)
 
